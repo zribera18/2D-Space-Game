f312c9c11e689971d685f6e978fc2c5e7e52dc66
Zachary Ribera <zribera18@gmail.com>
2016-11-21 00:47:34 -0800

    added instrucitons

--- Assets/loadOnClickScript.cs
+++ Assets/loadOnClickScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 public class loadOnClickScript : MonoBehaviour {
 GameObject[] pauseObjects;
+GameObject[] menuObjects;
 	// Use this for initialization
 	public void LoadScene(int level) {
   	SceneManager.LoadScene (level);
@@ -20,6 +21,14 @@ GameObject[] pauseObjects;
 		public void resume() {
 			Time.timeScale = 1;
 			hidePaused();
+		}
+
+		public void instructions() {
+			menuObjects = GameObject.FindGameObjectsWithTag("main_menu");
+			foreach(GameObject g in menuObjects){
+				g.SetActive(false);
+			}
+
 		}
 
     public void fin(int ignoreLevel)
